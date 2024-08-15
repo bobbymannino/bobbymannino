8caab8de81c999007b29efda41c62ea50a1e185c
Bob Man <manninobobby@icloud.com>
2024-08-15 15:06:37 +0100

    [chore] everything is back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,22 @@
     import type { Snippet } from 'svelte';
     import '../app.css'
     import Header from './header.svelte';
+    import Analytics from './analytics.svelte';
+    import Footer from './footer.svelte';
+    import Fonts from './fonts.svelte';
 
     type Props = {
         children: Snippet;
     };
 
     let { children }: Props = $props();
-    </script>
+  </script>
+
+<Analytics />
+<Fonts />
 
 <Header />
-{@render children()}
+<main>
+    {@render children()}
+</main>
+<Footer />
