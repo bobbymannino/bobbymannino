ee468e0306b7500d40abcd317f90922461f88a39
Bob Man <manninobobby@icloud.com>
2024-08-15 15:55:25 +0100

    feat: added the techs section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import AboutSection from "./sections.about.svelte";
+    import TechSection from "./sections.tech.svelte";
     import ProjectsSection from "./sections.projects.svelte";
     import ContactSection from "./sections.contact.svelte";
     import Meta from "$components/meta.svelte";
@@ -8,5 +9,6 @@
 <Meta title="Bobby Mannino" description="Software Engineering Undergraduate" />
 
 <AboutSection />
+<TechSection />
 <ProjectsSection />
 <ContactSection />
---
+++ src/routes/sections.tech.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    const techs = [
+        "typescript",
+        "git/github",
+        "html",
+        "css",
+        "svelte/kit",
+        "react/next",
+        "postgres",
+        "stripe",
+    ];
+</script>
+
+<section class="container" id="about">
+    <div class="card">
+        <h1>technologies</h1>
+
+        <p>
+            these are technologies i have an interest in personally or have some
+            knowledge of {#each techs as tech, i}<b>{tech}</b
+                >{#if i !== techs.length - 1},&nbsp;
+                {/if}{/each}
+        </p>
+
+        <p>
+            i am at the moment learning <b>swift</b> to help my ability to develops
+            ios apps as well as developing my own wrapper for a s3 bucket alternative,
+            as a university student i look for ways to save money and one way i found
+            was by creating my own solutions for next to nothing instead of paying
+            for a service
+        </p>
+    </div>
+</section>
