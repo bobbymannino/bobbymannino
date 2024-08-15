13b1f5f07707d4380304af986f6eaff22015eede
Bob Man <manninobobby@icloud.com>
2024-08-15 14:23:40 +0100

    contact section

--- src/routes/sections.contact.svelte
+++ src/routes/sections.contact.svelte
@@ -2,7 +2,21 @@
     <div class="card">
         <h1>contact</h1>
 
-        <h2>github</h2>
-        <h2>email</h2>
+        <hr />
+
+        <a
+            href="https://www.github.com/bobbymannino"
+            target="_blank"
+            class="hover:underline"
+        >
+            <h2>github</h2>
+        </a>
+        <a
+            href="mailto:manninobobby@icloud.com"
+            target="_blank"
+            class="hover:underline"
+        >
+            <h2>email</h2>
+        </a>
     </div>
 </section>
