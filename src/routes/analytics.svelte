8b5feee40e076477e9737bcbc319e202473afd12
Bob Man <manninobobby@icloud.com>
2024-08-15 14:28:24 +0100

    feat: Add analytics script to production environment.

---
+++ src/routes/analytics.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { dev } from "$app/environment";
+</script>
+
+<svelte:head>
+    {#if !dev}
+        <script
+            defer
+            src="https://analytics.bobman.dev/script.js"
+            data-website-id="5657dfa2-18a6-45d4-8ff6-1bd7ed17dd90"
+        ></script>
+    {/if}
+</svelte:head>
