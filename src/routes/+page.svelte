221f3ed3a57e1c816656d4c1c3f9d9bad78a009d
Bob Man <manninobobby@icloud.com>
2024-08-15 11:44:44 +0100

    [ui] added inter font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each { length: 8 } as _}
+    <div class="container">
+        <h1 class="font-bold text-3xl">Welcome to SvelteKit</h1>
+        <br />
+        <br />
+        <p>
+            Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+            documentation
+        </p>
+    </div>
+{/each}
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,5 +1,18 @@
-<header>
+<script lang="ts">
+    const links = [["#projects", "Projects"]];
+</script>
+
+<header class="backdrop-blur sticky top-0">
     <div class="container">
         <a href="/">bobman</a>
+        <nav>
+            <ul>
+                {#each links as [href, text]}
+                    <li>
+                        <a {href}>{text}</a>
+                    </li>
+                {/each}
+            </ul>
+        </nav>
     </div>
 </header>
