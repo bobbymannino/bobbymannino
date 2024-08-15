7839eeedf5f1ebb65324db294dfc128701889dd7
Bob Man <manninobobby@icloud.com>
2024-08-15 15:33:26 +0100

    meta for pages

---
+++ src/components/meta.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    type Props = {
+        title: string;
+        description: string;
+    };
+
+    let { title, description }: Props = $props();
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+
+    <meta name="description" content={description} />
+    <meta name="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
+</svelte:head>
