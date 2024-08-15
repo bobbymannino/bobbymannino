75cb347d585afd638eddc135a383340ed79f4d1a
Bob Man <manninobobby@icloud.com>
2024-08-15 16:45:08 +0100

    style: added console message for users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,12 @@
     import ProjectsSection from "./sections.projects.svelte";
     import ContactSection from "./sections.contact.svelte";
     import Meta from "$components/meta.svelte";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+         console.info("%c bobman.dev", "font-weight:900;font-size:2.5rem;color:white;background-color:blue")
+         console.warn("%c i dont think you should be here!", "font-size:1.2rem;background-color:amber")
+    })
 </script>
 
 <Meta title="Bobby Mannino" description="Software Engineering Undergraduate" />
