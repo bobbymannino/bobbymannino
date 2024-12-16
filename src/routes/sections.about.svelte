d2c25c96713decde7dc776072f9f62b900c8be36
bobbymannino <manninobobby@icloud.com>
2024-12-16 21:42:28 +0000

    breaking: remove fade effect

--- src/routes/sections.about.svelte
+++ src/routes/sections.about.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import Picture from "$components/picture.svelte";
-    import { fade } from "$lib/fade";
     import { hacker } from "$lib/hacker";
     import { inview } from "svelte-inview";
 </script>
@@ -10,10 +9,7 @@
         <h1
             use:inview
             on:inview_enter={(e) => {
-                const { node } = e.detail;
-
-                const clean = hacker(node);
-
+                const clean = hacker(e.detail.node);
                 return () => clean();
             }}
         >
@@ -22,16 +18,7 @@
 
         <Picture src="/images/lego-me" alt="Bobby Mannino" class="w-32" />
 
-        <p
-            use:inview
-            on:inview_enter={(e) => {
-                const { node } = e.detail;
-
-                const clean = fade(node);
-
-                return () => clean();
-            }}
-        >
+        <p>
             hey, im bob, i study software engineering in a university in the
             south of england. im 20 years old and absolutely in love with
             programming. my goal is to get a placement year in university and
@@ -39,16 +26,7 @@
             to help me land a job
         </p>
 
-        <p
-            use:inview
-            on:inview_enter={(e) => {
-                const { node } = e.detail;
-
-                const clean = fade(node, { delay: 1500 });
-
-                return () => clean();
-            }}
-        >
+        <p>
             for now i spend my spare time making mini projects and improving my
             skills. the current project im working on is a gym tracker. it will
             be able to track your workouts, create routines and give you
