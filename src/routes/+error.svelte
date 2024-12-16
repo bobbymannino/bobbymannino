6597b362b9adf6d45d164094afba702b473f9c78
bobbymannino <manninobobby@icloud.com>
2024-12-16 18:34:12 +0000

    ui: remove hacker effect from error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,22 +1,6 @@
-<script lang="ts">
-    import { hacker } from "$lib/hacker";
-    import { inview } from "svelte-inview";
-</script>
-
 <div class="container">
     <div class="card">
-        <h1
-            use:inview
-            on:inview_enter={(e) => {
-                const { node } = e.detail;
-
-                const clean = hacker(node);
-
-                return () => clean();
-            }}
-        >
-            Oh No :(
-        </h1>
+        <h1>Oh No :(</h1>
 
         <p>
             It seems we've encountered an error. Try refreshing the page or head <a
