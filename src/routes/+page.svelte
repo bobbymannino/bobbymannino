df12c53d9db81afa1c28bdd74ca2af81944a5c9a
Bobby Mannino <manninobobby@icloud.com>
2024-12-26 16:29:55 +0000

    feat: add blog section to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import TechSection from "./sections.tech.svelte";
   import ProjectsSection from "./sections.projects.svelte";
   import ContactSection from "./sections.contact.svelte";
+  import BlogSection from "./sections.blog.svelte";
   import Meta from "$components/meta.svelte";
 </script>
 
@@ -12,3 +13,4 @@
 <TechSection />
 <ProjectsSection />
 <ContactSection />
+<BlogSection />
---
+++ src/routes/sections.blog.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { hacker } from "$lib/hacker";
+  import { inview } from "svelte-inview";
+</script>
+
+<section class="container" id="blog">
+  <div class="card">
+    <h1
+      use:inview
+      on:inview_enter={(e) => {
+        const clean = hacker(e.detail.node);
+        return () => clean();
+      }}
+    >
+      blog
+    </h1>
+
+    <p>
+      i also keep a <a href="/blog" class="text-accent-600 hover:underline"
+        >blog</a
+      > for things or projects that have interested me, to me its like a notebook
+      of sorts, written in such a way that i can understand and revise quicker then
+      i could reading somebody elses blog.
+    </p>
+  </div>
+</section>
