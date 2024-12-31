3d8c1ea7ce633e4fb0b7d7fe4225cc9f5e77ae12
Bobby Mannino <manninobobby@icloud.com>
2024-12-31 16:38:05 +0000

    feat: add code block syntax highlighting

---
+++ src/lib/renderers/code.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { Snippet } from "svelte";
+  import hljs from "highlight.js";
+  import "./code.css";
+
+  type Props = {
+    children: Snippet;
+    lang: string;
+    raw: string;
+    text: string;
+  };
+
+  let { text, lang }: Props = $props();
+
+  let html = $state(text);
+
+  onMount(() => {
+    const code = hljs.highlight(text, { language: lang });
+    html = code.value;
+  });
+</script>
+
+<pre><code class="hljs">{@html html}</code></pre>
