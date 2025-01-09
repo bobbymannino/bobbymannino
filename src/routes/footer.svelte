2627e35ac2cc0eef97c9b232dffa5829e0895296
Bobby Mannino <manninobobby@icloud.com>
2025-01-09 19:03:02 +0000

    ui: add repo link to footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { browser } from "$app/environment";
+  import GithubIcon from "$lib/icons/github.svelte";
 
   const cmdOrCtrl = $derived(
     browser && navigator.platform.includes("Mac") ? "⌘" : "Ctrl",
@@ -8,9 +9,19 @@
 
 <footer class="bg-white dark:bg-zinc-900">
   <div class="wrap container flex items-center justify-between">
-    <p>
-      <small>&copy; bobman.dev 2024</small>
-    </p>
+    <div class="flex items-center gap-2">
+      <a
+        target="_blank"
+        href="https://www.github.com/bobbymannino/bobbymannino"
+        class="hover:text-accent-600 text-zinc-800 dark:text-zinc-100"
+        title="See this repo on GitHub"
+      >
+        <GithubIcon class="size-6" />
+      </a>
+      <p>
+        <small>&copy; bobman.dev 2024</small>
+      </p>
+    </div>
     <div class="not-hoverable:hidden">
       <kbd>{cmdOrCtrl}</kbd>
       <kbd>K</kbd>
