22b3e1302b04ee67b3ccd938403786de7cd9a4a0
Bobby Mannino <manninobobby@icloud.com>
2025-04-26 15:43:45 +0100

    🐛 fix: code colors in table
    fixes #27

--- src/lib/renderers/codespan.svelte
+++ src/lib/renderers/codespan.svelte
@@ -9,5 +9,6 @@
   let { text }: Props = $props();
 </script>
 
-<code class="inline-block bg-zinc-100 px-1 py-0.5 dark:bg-zinc-800">{text}</code
->
+<code class="inline-block bg-zinc-100 px-1 py-0.5 dark:bg-zinc-800">
+  {text}
+</code>
