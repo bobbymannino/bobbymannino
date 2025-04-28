e93223d105cf36576390a1b2b2ae6d3686a80d05
Bobby Mannino <manninobobby@icloud.com>
2025-04-28 22:59:47 +0100

    update meta

--- src/routes/meta.svelte
+++ src/routes/meta.svelte
@@ -7,6 +7,13 @@
   <link rel="icon" type="image/png" href="/favicon.png" />
   <meta name="author" content="Bobby Mannino" />
   <meta property="og:site_name" content="Bobby Mannino" />
-  <meta property="og:url" content="{PUBLIC_URL}{page.url.pathname}" />
+  <meta name="robots" content="index, follow" />
+  <meta
+    property="og:url"
+    content="{PUBLIC_URL}{page.url
+      .pathname}?${page.url.searchParams.toString()}"
+  />
+  <meta property="og:locale" content="en_GB" />
   <meta name="twitter:site" content="@bobbymannin0" />
+  <link rel="canonical" href="{PUBLIC_URL}{page.url.pathname}" />
 </svelte:head>
