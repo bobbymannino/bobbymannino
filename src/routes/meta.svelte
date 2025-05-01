9ad7920773fee600fb80f0481d2cf115659ef12a
Bobby Mannino <manninobobby@icloud.com>
2025-05-01 17:35:48 +0100

    🐛 fix: searchParams not accessible on pre-rendered page

--- src/routes/meta.svelte
+++ src/routes/meta.svelte
@@ -8,11 +8,7 @@
   <meta name="author" content="Bobby Mannino" />
   <meta property="og:site_name" content="Bobby Mannino" />
   <meta name="robots" content="index, follow" />
-  <meta
-    property="og:url"
-    content="{PUBLIC_URL}{page.url
-      .pathname}?${page.url.searchParams.toString()}"
-  />
+  <meta property="og:url" content="{PUBLIC_URL}{page.url.pathname}" />
   <meta property="og:locale" content="en_GB" />
   <meta name="twitter:site" content="@bobbymannin0" />
   <link rel="canonical" href="{PUBLIC_URL}{page.url.pathname}" />
