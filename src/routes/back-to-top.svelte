97d5cca7e62c8a1fa4c544b351c3c7f9c326a75b
Bobby Mannino <manninobobby@icloud.com>
2025-05-07 12:12:53 +0100

    🎨 ui: minor ui changes to back to top button

--- src/routes/back-to-top.svelte
+++ src/routes/back-to-top.svelte
@@ -9,7 +9,7 @@
     in:fly={{ y: 16 }}
     out:fly={{ y: -16 }}
     onclick={() => window.scrollTo({ top: 0 })}
-    class="text-accent-600 fixed bottom-4 left-4 cursor-pointer bg-white p-1 hover:bg-zinc-50 dark:bg-zinc-900 dark:hover:bg-zinc-800"
+    class="text-accent-600 ring-on-focus-visible fixed bottom-2 left-2 cursor-pointer bg-white p-1 hover:bg-zinc-50 dark:bg-zinc-900 dark:hover:bg-zinc-800"
   >
     <span class="sr-only">Scroll to top</span>
     <ChevronUpIcon />
