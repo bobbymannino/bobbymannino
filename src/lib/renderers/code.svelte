990654e1c81d138a2e0e76d3a3f4953cd7d72ec1
Bob Mannino <manninobobby@icloud.com>
2026-02-06 15:59:14 +0000

    feat: show copied state and icon in code renderer

--- src/lib/renderers/code.svelte
+++ src/lib/renderers/code.svelte
@@ -5,6 +5,7 @@
   import DuplicateIcon from "$lib/icons/dupliate-icon.svelte";
   import "./code.css";
   import { copyTextToClipboard } from "$lib/utils";
+  import CheckIcon from "$lib/icons/check-icon.svelte";
 
   type Props = {
     children: Snippet;
@@ -16,11 +17,22 @@
   let { text, lang }: Props = $props();
 
   let html = $derived(text);
+  let copied = $state(false);
+  let timer: null | NodeJS.Timeout = $state(null);
 
   onMount(() => {
     const code = hljs.highlight(text, { language: lang });
     html = code.value;
   });
+
+  function copy() {
+    copied = true;
+    copyTextToClipboard(text);
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      copied = false;
+    }, 2000);
+  }
 </script>
 
 <div>
@@ -29,12 +41,17 @@
   >
     <span class="uppercase">{lang}</span>
     <button
-      onclick={() => copyTextToClipboard(text)}
+      onclick={copy}
       tabindex="0"
       class="hover:text-accent-600 ring-on-focus-visible active:text-accent-700 flex cursor-pointer items-center gap-1 active:scale-95"
     >
-      <DuplicateIcon class="size-4" />
-      <span>Copy</span>
+      {#if copied}
+        <CheckIcon class="size-4" />
+        <span>Copied</span>
+      {:else}
+        <DuplicateIcon class="size-4" />
+        <span>Copy</span>
+      {/if}
     </button>
   </div>
   <pre><code class="hljs">{@html html}</code></pre>
