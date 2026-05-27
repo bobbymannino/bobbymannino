daac0c2074cd2e5541886bda492f4c49df930856
Bob Mannino <manninobobby@icloud.com>
2026-05-27 13:46:36 +0100

    feat: copy page link when clicking a header on a post

--- src/lib/renderers/heading.svelte
+++ src/lib/renderers/heading.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
+  import { copyTextToClipboard } from "$lib/utils";
   import { textToId } from "../../routes/blog/[slug]/+page.svelte";
 
   type Props = {
@@ -11,10 +12,14 @@
   let { children, depth, text }: Props = $props();
 
   const id = $derived(textToId(text));
+
+  function copyLink() {
+    copyTextToClipboard(`${window.location.origin}${window.location.pathname}#${id}`);
+  }
 </script>
 
 <svelte:element this={`h${depth}`} {id} style={depth == 1 ? `--vtn: post-title-${id}` : ""}>
-  <a href="#{id}" class="ring-on-focus-visible hover:underline" tabindex="0">
+  <a href="#{id}" onclick={copyLink} class="ring-on-focus-visible hover:underline" tabindex="0">
     {@render children()}
   </a>
 </svelte:element>
