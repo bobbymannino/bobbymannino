3368ce2fa8fa87e3f09fd6017826727ebe42a42b
Bob Mannino <manninobobby@icloud.com>
2026-05-29 07:47:25 +0100

    fix: add meta to /blog/tags

--- src/routes/blog/tags/+page.svelte
+++ src/routes/blog/tags/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import ChipSelection from "$components/chip-selection.svelte";
+  import Meta from "$components/meta.svelte";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 
   const chips = $derived(data.tags.map((t) => ({ text: `#${t}`, value: `/blog/tags/${t}` })));
 </script>
+
+<Meta title="Blog Tags | Bobby Mannino" description="Filter blog posts by tags" />
 
 <section class="container" id="tags">
   <div class="card">
