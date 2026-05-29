d5c1be24b1488ac96db825e0562aecefddec7c4b
Bob Mannino <manninobobby@icloud.com>
2026-05-29 07:40:42 +0100

    add series type

---
+++ src/lib/posts/series.ts
@@ -0,0 +1,14 @@
+type Series = {
+  title: string;
+  slug: string;
+  description: string;
+};
+
+/** A list of blog series */
+export const series: Series[] = [
+  {
+    title: "Which Editor",
+    slug: "which-editor",
+    description: "Every year or so I compare the most popular IDEs out there and explain what i'm currently using",
+  },
+];
