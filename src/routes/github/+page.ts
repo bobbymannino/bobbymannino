a4bd300bee62595c43db7105d92d48df312f7f1b
Bob Mannino <manninobobby@icloud.com>
2026-07-25 13:34:48 +0100

    fix: use see other (303) for social redirects

--- src/routes/github/+page.ts
+++ src/routes/github/+page.ts
@@ -1,4 +1,4 @@
 import { GITHUB_URL } from "$app/env/public";
 import { redirect } from "@sveltejs/kit";
 
-export const load = () => redirect(308, GITHUB_URL);
+export const load = () => redirect(303, GITHUB_URL);
--- src/routes/linkedin/+page.ts
+++ src/routes/linkedin/+page.ts
@@ -1,4 +1,4 @@
 import { LINKEDIN_URL } from "$app/env/public";
 import { redirect } from "@sveltejs/kit";
 
-export const load = () => redirect(308, LINKEDIN_URL);
+export const load = () => redirect(303, LINKEDIN_URL);
