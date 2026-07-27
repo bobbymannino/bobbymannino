e9ac5b11a93bb74315f437212520389f0a20df13
Bob Mannino <manninobobby@icloud.com>
2026-07-27 08:24:05 +0100

    docs: update about section

--- src/routes/sections.about.svelte
+++ src/routes/sections.about.svelte
@@ -26,7 +26,7 @@
     <p>
       hey, <span class="highlight">im <b>bob</b></span>, i study software engineering in a university in the south of
       england. i am
-      <span class="highlight group-hover/highlight:delay-200">21 years old</span>
+      <span class="highlight group-hover/highlight:delay-200">22 years old</span>
       and absolutely
       <span class="highlight group-hover/highlight:delay-400">love programming!</span> my goal is spend this academic year
       learning the most out of my placement so when the time comes (post uni) i am prepared to get a fulltime job
@@ -34,8 +34,8 @@
 
     <p>
       for now i spend my spare time making mini projects and improving my skills. the current project im working on is
-      an alternative to <a target="_blank" rel="noopener noreferrer" href="https://plane.so">plane.so</a>, aimed at
-      being something i can and will use in my daily life to track projects
+      called <a target="_blank" rel="noopener noreferrer" href="https://github.com/bobbymannino/sapere">Sapere</a>, it
+      is aimed at being something i can and will use in my daily life to track projects, issues and store documents
     </p>
   </div>
 </section>
