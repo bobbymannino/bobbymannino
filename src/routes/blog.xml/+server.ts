fc35dbe47ca5f1e16de1e15c227d5f9b451da594
Bob Mannino <manninobobby@icloud.com>
2026-09-08 10:42:54 +0100

    fix: use marked for showing blog post content correctly

--- src/routes/blog.xml/+server.ts
+++ src/routes/blog.xml/+server.ts
@@ -1,13 +1,14 @@
 import { URL as URLS } from "$app/env/public";
 import { listPosts } from "$lib/posts";
 import type { Post } from "$lib/posts";
+import { marked } from "marked";
 
 const title = escapeXml("Bobby Mannino's Blog");
 const description = escapeXml("Some things I have learnt and would like to remember");
 const URL = URLS.split(",")[0];
 
 const preXml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>${title}</title>
     <description>${description}</description>
@@ -30,8 +31,20 @@ function escapeXml(raw: string) {
     .replace(/'/g, "&apos;");
 }
 
+/** Wraps content in CDATA so readers receive it as markup, not escaped text */
+function cdata(raw: string) {
+  return `<![CDATA[${raw.replace(/]]>/g, "]]]]><![CDATA[>")}]]>`;
+}
+
+/** Renders a post's markdown to HTML, with root-relative URLs made absolute */
+function renderContent(content: string) {
+  const html = marked.parse(content, { async: false, gfm: true });
+
+  return html.replace(/(href|src)="\/(?!\/)/g, `$1="${URL}/`);
+}
+
 function postToXml(post: Post) {
-  const { title, publishedOn, slug } = post.meta;
+  const { title, tagline, publishedOn, slug } = post.meta;
   const url = `${URL}/blog/${slug}`;
 
   return `
@@ -40,7 +53,8 @@ function postToXml(post: Post) {
           <link>${url}</link>
           <guid isPermaLink="false">bobmandev:blog:${slug}</guid>
           <pubDate>${publishedOn.toUTCString()}</pubDate>
-          <description>${escapeXml(post.content)}</description>
+          <description>${escapeXml(tagline)}</description>
+          <content:encoded>${cdata(renderContent(post.content))}</content:encoded>
         </item>
       `;
 }
