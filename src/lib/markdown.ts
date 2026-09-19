832a6d10c74b531db1768428cdf0ed6f9e7ef884
Bob Mannino <manninobobby@icloud.com>
2026-09-19 14:57:53 +0100

    fix: use correct icons in markdown

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,6 +1,9 @@
 import { textToId, type Heading } from "$lib/headings";
+import { CheckIcon, DuplicateIcon } from "$lib/icons";
 import hljs from "highlight.js";
 import { Marked, Renderer } from "marked";
+import type { Component } from "svelte";
+import { render } from "svelte/server";
 
 function escapeHtml(raw: string) {
   return raw
@@ -11,9 +14,13 @@ function escapeHtml(raw: string) {
     .replace(/'/g, "&#39;");
 }
 
-const CHECK_ICON = `<svg class="size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.3209 4.24472C20.0143 4.69807 20.2088 5.62768 19.7555 6.32105L11.2555 19.321C10.9972 19.7161 10.5681 19.9665 10.0971 19.997C9.62616 20.0276 9.16828 19.8347 8.86114 19.4764L4.36114 14.2264C3.82201 13.5974 3.89485 12.6504 4.52384 12.1113C5.15283 11.5722 6.09978 11.645 6.63891 12.274L9.83828 16.0066L17.2446 4.6793C17.6979 3.98593 18.6275 3.79136 19.3209 4.24472Z" fill="currentColor"/></svg>`;
+/** Renders an icon component to an HTML string, so markdown HTML stays in sync with the icon package */
+function renderIcon(Icon: Component<Record<string, unknown>>) {
+  return render(Icon, { props: { class: "size-4" } }).body;
+}
 
-const DUPLICATE_ICON = `<svg class="size-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 8V5.25C8 3.45508 9.45507 2 11.25 2H18.75C20.5449 2 22 3.45507 22 5.25V12.75C22 14.5449 20.5449 16 18.75 16H16V18.75C16 20.5449 14.5449 22 12.75 22H5.25C3.45507 22 2 20.5449 2 18.75V11.25C2 9.45508 3.45507 8 5.25 8H8ZM11.25 16C9.45507 16 8 14.5449 8 12.75V10H5.25C4.55964 10 4 10.5596 4 11.25V18.75C4 19.4404 4.55964 20 5.25 20H12.75C13.4404 20 14 19.4404 14 18.75V16H11.25Z" fill="currentColor"/></svg>`;
+const CHECK_ICON = renderIcon(CheckIcon);
+const DUPLICATE_ICON = renderIcon(DuplicateIcon);
 
 const md = new Marked({ gfm: true }).use({
   renderer: {
